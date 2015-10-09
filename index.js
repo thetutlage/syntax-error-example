137ae4f6a70e86b887c98a21e2629c5be50b31c1
Harminder Virk <virk.officials@gmail.com>
2015-10-09 23:29:20 +0530

    funny stack constructor

--- index.js
+++ index.js
@@ -3,13 +3,21 @@
 var check = require('syntax-error')
 var stackTrace = require('stack-trace');
 var fs = require('fs')
+var http = require('http')
+
+http.createServer(function (req,res) {
+
+})
 
 var file = __dirname + '/foo.js';
 var src = fs.readFileSync(file);
 var err = check(src, file);
+
+var errorsArray = err.toString().split("\n")
+var stack = file + ":" + err.line + ":" + err.column
+err.stack = err.toString() + "\n at "+errorsArray[2]+" ("+stack+")"
 var trace = stackTrace.parse(err);
 console.log(trace)
-
 
 //////////////////////////////////////
 // Below one has proper stack trace //
